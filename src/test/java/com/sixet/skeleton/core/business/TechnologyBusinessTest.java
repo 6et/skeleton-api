de77a6ceb669261176fca10e281e14f2f80bf1fe
Get Trevisan <gtrevisane@gmail.com>
2019-06-06 10:48:09 -0300

    Removing unused imports

--- src/test/java/com/sixet/skeleton/core/business/TechnologyBusinessTest.java
+++ src/test/java/com/sixet/skeleton/core/business/TechnologyBusinessTest.java
@@ -8,13 +8,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.isA;
