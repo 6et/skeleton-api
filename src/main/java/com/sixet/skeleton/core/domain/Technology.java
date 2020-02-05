b86be9099ca66afd28dbb3966e3559b853757eef
Get Trevisan <gtrevisane@gmail.com>
2020-02-05 20:48:31 -0300

    Ajustando

--- src/main/java/com/sixet/skeleton/core/domain/Technology.java
+++ src/main/java/com/sixet/skeleton/core/domain/Technology.java
@@ -1,18 +1,12 @@
 package com.sixet.skeleton.core.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 import org.springframework.util.StringUtils;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 /**
